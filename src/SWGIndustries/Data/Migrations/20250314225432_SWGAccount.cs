2cbae920f71f247004bd485e353d30d55f5337df
nockawa <loic.baumann@outlook.com>
2025-03-16 18:02:26 +0100

    Grooming...

---
+++ src/SWGIndustries/Data/Migrations/20250314225432_SWGAccount.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SWGIndustries.data.migrations
+{
+    /// <inheritdoc />
+    public partial class SWGAccount : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SWGAccounts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 30, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SWGAccounts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SWGCharacters",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 30, nullable: true),
+                    AccountId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SWGCharacters", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SWGCharacters_SWGAccounts_AccountId",
+                        column: x => x.AccountId,
+                        principalTable: "SWGAccounts",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SWGCharacters_AccountId",
+                table: "SWGCharacters",
+                column: "AccountId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SWGCharacters");
+
+            migrationBuilder.DropTable(
+                name: "SWGAccounts");
+        }
+    }
+}
